e37890f72cdc0dbaac8cbd22f0447cffe660ca92
terminalj <terminal_j@protonmail.com>
2026-04-09 19:40:27 +0200

    Added RecyclerView to Score and Landscape View to both Game and Score Activities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/simonprototype/ActivityGameFragment.kt
+++ app/src/main/java/com/example/simonprototype/ActivityGameFragment.kt
@@ -8,6 +8,14 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import android.os.Build
+import android.content.res.Configuration
+import android.view.WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+
 class ActivityGameFragment : Fragment()
 {
     // View initialization logic
@@ -23,10 +31,18 @@ class ActivityGameFragment : Fragment()
     // Post view initialization logic
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
+//        val tv : TextView = findTextView(R.id.TextGame)
+
         val next : Button = view.findViewById(R.id.Button1)
         next.setOnClickListener {
             // Note: no result is coming back
             findNavController().navigate(R.id.activity_score)
+        }
+
+        val red : Button = view.findViewById(R.id.Button_1)
+        red.setOnClickListener {
+            val a = getString(R.string.sequence) + "R"
+
         }
     }
 }
